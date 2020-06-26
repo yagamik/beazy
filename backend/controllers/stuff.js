b7107927ffe645e1585f16cc9016ba284a826cae
yagamik <yannatkk@gmail.com>
2020-06-26 17:00:04 +0100

    modifying route DELETE configuration to delete an image

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,4 +1,6 @@
 const Thing = require('../models/Thing');
+const fs = require('fs');
+const { error } = require('console');
 
 exports.createThing = (req, res, next) => {
     const thingObject = JSON.parse(req.body.thing);
@@ -14,7 +16,7 @@ exports.createThing = (req, res, next) => {
 
 exports.modifyThing = (req, res, next) => {
     const thingObject = req.file ?
-        { 
+        {
             ...JSON.parse(req.body.thing),
             imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
         } : { ...req.body };
@@ -24,9 +26,16 @@ exports.modifyThing = (req, res, next) => {
 };
 
 exports.deleteThing = (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-        .catch(error => res.status(404).json({ error }));
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => {
+            const filename = thing.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                Thing.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+                    .catch(error => res.status(404).json({ error }));
+            })
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 exports.getOneThing = (req, res, next) => {
